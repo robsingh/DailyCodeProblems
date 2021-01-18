20e038ca06a66fa92ca4312f8cb2fac908f329f3
robsingh <robie10895@gmail.com>
2021-01-18 15:11:56 -0500

    Update Pair_sum.py

--- Array/Pair_sum.py
+++ Array/Pair_sum.py
@@ -1,4 +1,5 @@
 #naive approach to find a pair in the list with the given sum
+#not an ideal solution, gives time complexity of o(n^2), auxillary space used by this program is O(1)
 
 def findPair(arr,sum):
   #consider each element except the last
@@ -16,3 +17,27 @@ def findPair(arr,sum):
     arr = [2,5,7,8,7]
     sum = 10
     findPair(arr,sum)
+    
+
+# O(nlog(n)) using Sorting:
+def findPair(arr,sum):
+  arr.sort() #sorting the list in ascending order
+  
+  (low,high) = (0,len(arr)-1) #two indices pointing to end-points of list
+  #loop till search space is exhausted
+  while low < high:
+    if arr[low] + arr[high] == sum:
+      print("Pair found!!")
+      return 
+    if arr[low] + arr[high] < sum:
+      low += 1
+    else:
+      high -= 1
+      
+      
+  print("No pair found!")
+  
+if __name__ == "__main__":
+  arr = [8,7,2,5,3,1]
+  sum = 10
+  findPair(arr,sum)
