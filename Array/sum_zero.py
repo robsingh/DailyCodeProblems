51b17fd001be8fdefd197cbe8de8a7d9706afca5
robsingh <robie10895@gmail.com>
2021-01-23 13:30:49 -0500

    Update sum_zero.py

--- Array/sum_zero.py
+++ Array/sum_zero.py
@@ -1,2 +1,16 @@
 #given an integer array, print all subarrays with zero-sum
+#naive approach to solve this problem with time complexity O(n^3) 
 
+def printAllSublist(arr):
+    for i in range(len(arr)):
+        sum = 0
+        for j in range(i,len(arr)):
+            sum += arr[j]
+            if sum == 0:
+                print("Sublist: ",(i,j))
+
+if __name__ == "__main__":
+    arr = [3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
+    printAllSublist(arr)
+
+#second approach, using multimap to print all subarrays
