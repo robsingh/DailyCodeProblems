19042c42abbf3123cc836c3c8aeaa5c94f316577
robsingh <robie10895@gmail.com>
2024-04-19 14:25:25 -0400

    final commit, will update this repo soon

--- dpp_1077.py
+++ dpp_1077.py
@@ -1,6 +1,7 @@
 """This problem was asked by Apple.
 
-Given the root of a binary tree, find the most frequent subtree sum. The subtree sum of a node is the sum of all values under a node, including the node itself.
+Given the root of a binary tree, find the most frequent subtree sum. The subtree sum of a node is the sum of all values under a node, 
+including the node itself.
 
 For example, given the following tree:
 
@@ -8,4 +9,5 @@ For example, given the following tree:
  / \
 2  -5
 Return 2 as it occurs twice: once as the left leaf, and once as the sum of 2 + 5 - 5."""
+
 
