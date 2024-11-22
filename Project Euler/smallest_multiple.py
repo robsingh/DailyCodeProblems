c1524c40ac64eca7c77cdd77bf7560c85a04a11b
robsingh <robie10895@gmail.com>
2024-11-22 14:24:50 -0500

    cleaned some code

--- Project Euler/smallest_multiple.py
+++ Project Euler/smallest_multiple.py
@@ -5,25 +5,9 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 '''
 break number into prime factors.
-2 = 2
-3 = 3
-4 = 2^2
-5 = 5
-6 = 2*3
-7 = 7
-8 = 2^3
-...
-20 = 2^2*5
 take the highest power that appears in the prime factor.
-2^4 (16)
-3^2 (9)
-5^1 (5)
-7^1 (7)
-11,13,17,19 - prime nums
-
 product of these highest powers = LCM
 '''
-
 from math import gcd
 
 def lcm(a,b):
@@ -36,4 +20,3 @@ def lcm_range(n):
     return result
 
 print(lcm_range(20))
-
